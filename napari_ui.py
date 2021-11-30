f7e63a5f3ed84b8771aa9c1da10510fb7594623c
Haoyuan Li <jackie007hi@163.com>
2021-11-30 15:20:37 -0500

    'integrate_label'

--- napari_ui.py
+++ napari_ui.py
@@ -24,13 +24,76 @@ import argparse
 import time
 from PIL import Image
 from tqdm import tqdm
+import xml.etree.ElementTree as ET
+import numpy as np
+import pandas as pd
+from collections import Counter
+import matplotlib.pyplot as plt
+from shapely.geometry import box, Polygon
+from shapely.ops import cascaded_union
+from shapely.strtree import STRtree
+from shapely.geometry import MultiPolygon
 parser = argparse.ArgumentParser()
 parser.add_argument('--image_path', type=str, help='path of the input whole slide image')
 parser.add_argument('--model_path',type=str,help='path of the model to generate embedding')
 parser.add_argument('--prefix',type=str,help='path to store media result')
 parser.add_argument('--tile_size',type=int,help='tile size')
 parser.add_argument('--level',type=int,help='perform embedding at which level of image. the higher the level, the quicker the preprocess')
+parser.add_argument('--xml_path',type=str,help='the path of label xml file')
 args = parser.parse_args()
+def get_coords(tree):
+    """
+    input: 
+        tree: xml ElementTree object
+    output: 
+        polys: dictionary. key = annotation 0, 1, etc. value = polygon coordinates 
+        groups: dictionary. key = annotation 0, 1, etc. value = partofgroup: _0 or _2
+    """
+    
+    
+    root = tree.getroot() #root element
+    
+    polys = {} # key = annotation 0, 1, etc. value = polygon coordinates 
+    groups = {} # key = annotation 0, 1, etc. value = partofgroup: _0 or _2
+
+    for item in root.findall('Annotations/Annotation'):
+
+            # initialize for each Annotation group
+            x_co = []
+            y_co = []
+            name = item.get('Name') #retuns: Annotation 0,1,2,...
+            group = item.get('PartOfGroup') #returns: _0,_2
+
+            for child in item.findall('Coordinates/Coordinate'):
+
+                x = child.get('X') # returns: 0,1,2,
+                x_co.append(x)
+                y = child.get('Y') # returns: 0,1,2,
+                y_co.append(y)
+
+            # make into list of coords
+            x_co = np.array(x_co,dtype=np.float32)
+            y_co = np.array(y_co,dtype=np.float32)
+            coo = list(zip(y_co,x_co))
+
+            # save into dict
+            polys[name] = coo
+            groups[name] = group
+    
+    return polys, groups
+def tile_intersect_polys(query_geom,polygons):
+    """
+    This function checks if a tile overlaps with a group of polygons.
+    query_geom: a polygon object(tile). e.g. Polygon([(30000, 30000), (40000, 30000), (40000, 40000), (30000, 40000)])
+    polygons: a list of polygons(tumor). e.g. [Polygon(polys['Annotation 0']),...]
+    """
+    # create a shapely Tree object, but with query_geom
+    s = STRtree([query_geom])
+    # Returns a list of all geometries in the s(strtree) whose extents intersect the extent of multiple polygons
+    multi = MultiPolygon(polygons)
+    result = s.query(multi)
+    # does my query_geom intersect with the polygon? True/False
+    return query_geom in result
 def generate_pca_point(row,matrix,point_size):
     #print(matrix[0:10])
     matrix_min=np.min(matrix,axis=0)
@@ -85,7 +148,7 @@ def gen_cluster_mask(coords,row,col,orig_row,orig_col,plot_col,tile_size):
 def gen_mask(coords,row,col,orig_row,orig_col,plot_col,tile_size,line_width):
     lines=[]
     ld=0.5*float(line_width)
-    print(row/orig_row)
+    #print(row/orig_row)
     for i in range(len(coords)):
         media_x,media_y=transform_origpos(coords[i][0],coords[i][1],row,col,orig_row,orig_col,plot_col)
         media1_x,media1_y=transform_origpos(coords[i][0]+tile_size,coords[i][1]+tile_size,row,col,orig_row,orig_col,plot_col)
@@ -134,6 +197,11 @@ for i in os.listdir(args.prefix):
             break
 if not similar:
     print("no available preprocessed data, run preprocessing from begining")
+    polygons=None
+    if args.xml_path is not None:
+        tree = ET.parse(args.xml_path)
+        polys, groups = get_coords(tree) 
+        polygons = [Polygon(polys[k]) for k in polys.keys()]
     level_app=args.level
     if level_app is not None:
         level_app=min(level_app,wsi.slide.slide.level_count-1)
@@ -145,23 +213,6 @@ if not similar:
     tissue_mask_high_level = tissue_detector.F(high_level)
     ratio=int(2**(level_i-level_app))
     #print(ratio)
-    """
-    tissue_mask_high_level=Image.fromarray(tissue_mask_high_level)
-    tissue_mask_full_level=tissue_mask_high_level.resize(wsi.slide.get_image_shape(level_app))
-    tissue_mask_full_level=np.array(tissue_mask_full_level,dtype='uint8')
-    tissue_mask_full_level=tissue_mask_full_level.transpose()
-    del(tissue_mask_high_level)
-    wsi.masks.add("tissue", tissue_mask_full_level)
-    pipeline = Pipeline([])
-    wsi.run(pipeline, distributed=False,tile_size = args.tile_size,level=level_app)
-    tissue_masks=[]
-    for i in wsi.tiles:
-        tissue_masks.append(int(np.sum(i.masks['tissue']!=0)))
-    num_tiles = len(wsi.tiles)
-    coords = []
-    for i in range(num_tiles):
-        coords.append(wsi.tiles[i].coords)
-    """
     ###load Model
     model1 = torchvision.models.__dict__['resnet18'](pretrained=False)
     state = torch.load(args.model_path, map_location=torch.device('cpu'))
@@ -202,11 +253,12 @@ if not similar:
         return o_pooled_flat
 
     # make into a function
-    def get_features_wsi_eval(wsi,model,level,tissue_mask,ratio,tile_size):
+    def get_features_wsi_eval(wsi,model,level,tissue_mask,ratio,tile_size,polygons1=None):
         i=0
         j=0
         coords=[]
         embedding=[]
+        labels=[]
         ratio1=ratio*int(2**level)
         tile_size1=tile_size*int(2**level)
         for i in tqdm(range(0,wsi.slide.get_image_shape(0)[0],tile_size1)):
@@ -216,18 +268,20 @@ if not similar:
                     o_pooled_flat=get_features_eval(media,model)
                     embedding.append(o_pooled_flat[0])
                     coords.append([i,j])
+                    if polygons is not None:
+                        media_coord=[(i,j),(i+tile_size1,j),(i+tile_size1,j+tile_size1),(i,j+tile_size1)]
+                        query_p = Polygon(media_coord)
+                        labels.append(tile_intersect_polys(query_p,polygons1))
                 j+=tile_size1
             j=0
             i+=tile_size1
-        return coords,embedding
+        return coords,embedding,labels
     with torch.no_grad():
-        coords,tile_ma = get_features_wsi_eval(wsi,model1,level_app,tissue_mask_high_level,ratio,args.tile_size)
-        _,tile_orig = get_features_wsi_eval(wsi,model_orig,level_app,tissue_mask_high_level,ratio,args.tile_size)
-    #assert len(coords)==len(tissue_masks)
-    #assert len(tile_ma)==len(tissue_masks)
-    #tile_ma=[tile_ma[i] for i in range(len(tile_ma)) if tissue_masks[i]>0]
-    #coords=[coords[i] for i in range((len(coords))) if tissue_masks[i]>0]
-    diction={'emb':tile_ma,'coords':coords,'args':args,'level':level_app,'emb_orig':tile_orig}
+        coords,tile_ma,labels = get_features_wsi_eval(wsi,model1,level_app,tissue_mask_high_level,ratio,args.tile_size,polygons)
+        _,tile_orig,_ = get_features_wsi_eval(wsi,model_orig,level_app,tissue_mask_high_level,ratio,args.tile_size)
+    if len(labels)==0:
+        labels=None
+    diction={'emb':tile_ma,'coords':coords,'args':args,'level':level_app,'emb_orig':tile_orig,'labels':labels}
     now=str(int(time.time()))
     file_name=args.image_path.split('/')[-1].split('.')[0]+now+'.pickle'
     with open(os.path.join(args.prefix,file_name),'wb') as file:
@@ -240,6 +294,7 @@ else:
         coords=media["coords"]
         level_app=media['level']
         tile_orig=media['emb_orig']
+        labels=media['labels']
 cur_n_clusters=5
 cur_orig=False
 #print(coords[0:5])
@@ -250,19 +305,23 @@ kmean=KMeans(n_clusters=cur_n_clusters, random_state=0).fit(tile_ma)
 tile_orig=np.array(tile_orig,dtype=float)
 pca_orig = PCA(n_components=2)
 pca_matrix_orig=pca.fit_transform(tile_orig)
-#kmean_orig=KMeans(n_clusters=cur_n_clusters, random_state=0).fit(tile_orig)
-ratio=wsi.shape[0]/wsi.shape[1]
-if ratio>1:
-    thumbnail = wsi.slide.get_thumbnail(size = (10000,int(10000/ratio)))
-else:
-    thumbnail = wsi.slide.get_thumbnail(size = (int(10000*ratio),10000))
-color_list=['green','blue','orange','black','purple','yellow','pink']
+level_display=wsi.slide.slide.level_count-1
+for i in range(wsi.slide.slide.level_count-1):
+    if max(wsi.slide.get_image_shape(i))<15000:
+        level_display=i
+        break
+thumbnail=wsi.slide.extract_region(location=(0,0),level=level_display,size=wsi.slide.get_image_shape(level_display))
+color_list=['green','blue','orange','brown','purple','yellow','pink']
 point_size=int(0.005*thumbnail.shape[0])
 layer2_data,point_matrix,min_col,min_row,max_row,max_col,matrix_min,matrix_max=generate_pca_point(thumbnail.shape[0],pca_matrix,point_size)
 layer2_data_orig,point_matrix_orig,min_col_orig,min_row_orig,max_row_orig,max_col_orig,matrix_min_orig,matrix_max_orig=generate_pca_point(thumbnail.shape[0],pca_matrix_orig,point_size)
 line_width=8
-#print(thumbnail.shape)
 polys=gen_cluster_mask(coords,thumbnail.shape[0],thumbnail.shape[1],wsi.slide.get_image_shape(0)[0],wsi.slide.get_image_shape(0)[1],layer2_data.shape[1],args.tile_size*int(2**level_app))
+if labels is not None:
+    coords_label=[coords[i] for i in range(len(coords)) if labels[i]]
+    polys_label=gen_cluster_mask(coords_label,thumbnail.shape[0],thumbnail.shape[1],wsi.slide.get_image_shape(0)[0],wsi.slide.get_image_shape(0)[1],layer2_data.shape[1],args.tile_size*int(2**level_app))
+    polys_label_orig=gen_cluster_mask(coords_label,thumbnail.shape[0],thumbnail.shape[1],wsi.slide.get_image_shape(0)[0],wsi.slide.get_image_shape(0)[1],layer2_data_orig.shape[1],args.tile_size*int(2**level_app))
+polys_orig=gen_cluster_mask(coords,thumbnail.shape[0],thumbnail.shape[1],wsi.slide.get_image_shape(0)[0],wsi.slide.get_image_shape(0)[1],layer2_data_orig.shape[1],args.tile_size*int(2**level_app))
 polys_orig=gen_cluster_mask(coords,thumbnail.shape[0],thumbnail.shape[1],wsi.slide.get_image_shape(0)[0],wsi.slide.get_image_shape(0)[1],layer2_data_orig.shape[1],args.tile_size*int(2**level_app))
 viewer = napari.Viewer()
 p_color_list=[color_list[i] for i in kmean.labels_]
@@ -271,9 +330,12 @@ layer2=viewer.add_points(point_matrix,size=point_size,edge_color=p_color_list,na
 layer3=viewer.add_points(point_matrix[0:1],size=point_size*3,symbol='cross',opacity=0,edge_color='red',face_color='red',name='cross')
 layer4=viewer.add_shapes(polys,edge_width=0,face_color=p_color_list,opacity=0.5,name='tile')
 layer5=viewer.add_shapes(np.array([[1114,2997.5],[1114,3064],[1178,3064],[1178,3000],[1111.5,3000]]),edge_color='red',edge_width=line_width,shape_type='path',opacity=0.0,name='grid')
+if labels is not None:
+    layer6=viewer.add_shapes(polys_label,edge_width=0,face_color='black',opacity=0.5,name='cancer label')
 layer1.editable=False
 layer3.editable=False
 layer5.editable=False
+layer6.editable=False
 def update_grid_position(event):
     if len(event.source.selected_data)>0:
         media=list(event.source.selected_data)
@@ -326,28 +388,25 @@ def radio(radio_option=False):
         layer5.opacity=0.0
         if cur_orig:
             kmean=KMeans(n_clusters=cur_n_clusters, random_state=0).fit(tile_orig)
-            print(kmean.labels_[0:20])
+            #print(kmean.labels_[0:20])
             p_color_list=[color_list[i] for i in kmean.labels_]
             layer1.data=np.concatenate((layer2_data_orig,thumbnail),axis=1)
             layer2.data=point_matrix_orig
             layer2.edge_color=p_color_list
             layer4.data=polys_orig
             layer4.face_color=p_color_list
-            
+            if labels is not None:
+                layer6.data=polys_label_orig
         else:
             kmean=KMeans(n_clusters=cur_n_clusters, random_state=0).fit(tile_ma)
-            print(kmean.labels_[0:20])
+            #print(kmean.labels_[0:20])
             p_color_list=[color_list[i] for i in kmean.labels_]
             layer1.data=np.concatenate((layer2_data,thumbnail),axis=1)
             layer2.data=point_matrix
             layer2.edge_color=p_color_list
             layer4.data=polys
             layer4.face_color=p_color_list
-    """        
-    kmean=KMeans(n_clusters=slider_int, random_state=0).fit(tile_ma)
-    p_color_list=[color_list[i] for i in kmean.labels_]
-    layer2.edge_color=p_color_list
-    layer4.face_color=p_color_list
-    """
+            if labels is not None:
+                layer6.data=polys_label
 viewer.window.add_dock_widget(radio)
 napari.run()
